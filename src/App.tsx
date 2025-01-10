5fad10450af1a306c9009e6ff7da5498f621f6c9
m4gler <przedosiaczek@gmail.com>
2025-01-10 20:35:38 +0100

    komitdsgf

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './index.css';
+import { MainPage } from './pages/MainPage';
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
