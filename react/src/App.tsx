15169ee264e231965bfd2ce3f6b7a061caadc31d
m4gler <przedosiaczek@gmail.com>
2025-01-10 18:28:21 +0100

    css

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './index.css';
-import './App.css';
 import { MainPage } from './pages/MainPage';
 
 function App() {
